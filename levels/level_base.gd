abc8f7099119b64816ad17597c8f2913c367d318
lingdong <lingdong@bantouyan.com>
2026-07-06 16:03:28 +0800

    Spawn player at spawn point

--- levels/level_base.gd
+++ levels/level_base.gd
@@ -3,8 +3,10 @@ extends Node2D
 
 @onready var player = $Player
 @onready var hud = $HUD
+@onready var spawn_point = $SpawnPoint
 
 
 func _ready() -> void:
+	player.global_position = spawn_point.global_position
 	hud.set_health(player.health)
 	player.hurt_taken.connect(hud.decrease_health)
